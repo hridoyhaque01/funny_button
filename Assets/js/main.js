a2c764cc34ea397b8cea08530f8a7088346c88f2
Hridoy <iamnirob334@gmail.com>
2022-10-08 07:52:44 +0600

    funny button source code uploaded

---
+++ Assets/js/main.js
@@ -0,0 +1,52 @@
+// manual database
+
+const dbEmail = "iamhridoyhaque@gmail.com";
+const dbPass = "demoPassword";
+
+const form = document.getElementById("form"),
+  email = document.querySelector(".email"),
+  password = document.querySelector(".password"),
+  button = document.querySelector(".loged");
+
+let boxes = true;
+
+if (boxes) {
+}
+
+const unvalid = (e) => {
+  e.target.style.borderColor = "#fa3f67";
+  form.classList.add("unvalid");
+};
+
+const valid = (e) => {
+  e.target.style.borderColor = "#4239c5";
+  form.classList.remove("unvalid");
+  button.classList.remove("wrong");
+};
+
+email.addEventListener("keyup", (e) => {
+  let inputValue = e.target.value;
+
+  if (inputValue !== dbEmail) {
+    unvalid(e);
+  } else {
+    valid(e);
+  }
+});
+
+password.addEventListener("keyup", (e) => {
+  let inputValue = e.target.value;
+  if (inputValue !== dbPass) {
+    unvalid(e);
+  } else {
+    valid(e);
+  }
+});
+
+button.addEventListener("mouseover", (e) => {
+  if (form.classList.contains("unvalid")) {
+    e.target.classList.toggle("wrong");
+  } else {
+    e.target.classList.remove("wrong");
+  }
+});
